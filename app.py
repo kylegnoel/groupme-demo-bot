af1857bfe280bf847d61550eea3c9d6349a9a0a2
lsw <leong16@purdue.edu>
2021-02-22 18:38:55 -0500

    fix

--- app.py
+++ app.py
@@ -1,33 +1,4 @@
 import os
-import json
-
-from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/', methods=['POST'])
-def webhook():
-    data = request.get_json()
-
-    if data['name'] != 'lsw-groupme-demo-bot':
-        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-        send_message(msg)
-
-    return "ok", 200
-
-def send_message(msg):
-    url = 'https://api.groupme.com/v3/bots/post'
-
-    data = {
-            'bot_id' : os.getenv('GROUPME_BOT_ID'),
-            'text'   : msg,
-            }
-
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()import os
 import sys
 import json
 
@@ -44,7 +15,7 @@ def webhook():
   log('Recieved {}'.format(data))
 
   # We don't want to reply to ourselves!
-  if data['name'] != 'apnorton-test-bot':
+  if data['name'] != 'lsw-groupme-demo-bot':
     msg = '{}, you sent "{}".'.format(data['name'], data['text'])
     send_message(msg)
 
@@ -59,9 +30,7 @@ def send_message(msg):
          }
   request = Request(url, urlencode(data).encode())
   json = urlopen(request).read().decode()
-
+  
 def log(msg):
   print(str(msg))
   sys.stdout.flush()
-
-
